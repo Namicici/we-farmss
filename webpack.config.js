6b5919523290e136f6578418ba78fd543190995f
Namicici <847954654@qq.com>
2016-02-01 10:29:10 +0800

    Change package.json and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,8 @@
-
-module.exports = function(webpackConfig) {
-    webpackConfig.module.loaders.forEach(function(loader) {
-        if (loader.loader === 'babel') {
-            // https://github.com/ant-design/babel-plugin-antd
-            loader.query.plugins.push('antd');
-
-            loader.loaders = ['es3ify', 'babel?'+JSON.stringify(loader.query)];
-            delete loader.query;
-            delete loader.loader;
-        }
-        return loader;
-    });
-
-    /*webpackConfig.entry = ["./src/entry/index.jsx"];
-    webpackConfig.output = {
-        path: __dirname + "/src/",
-        publicPath: "/dist/",
-        filename: "app.js"
-    };*/
-
-    return webpackConfig;
+var path = require("path");
+module.exports = {
+    entry: ['webpack/hot/dev-server', path.resolve(__dirname, "app/main.js")],
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js"
+    }
 };
