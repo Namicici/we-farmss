3728b188b6d460fa543c6ca829884387eba6c6ca
Namicici <847954654@qq.com>
2016-08-31 18:31:37 +0800

    add inline style

--- src/app/common/bottom-tab.js
+++ src/app/common/bottom-tab.js
@@ -3,11 +3,30 @@ import {Link} from 'react-router';
 
 export default class BottomTab extends Component {
     render() {
+        var footerStyle={
+            tab:{
+                'width':'100%',
+                'position':'fixed',
+                'bottom':0,
+                'left':0,
+                'height':'32px',
+                'background':'mediumaquamarine',
+                'padding-top': '8px'
+            },
+            item:{
+                'width':'33%',
+                'display':'inline-block',
+                'text-align':'center',
+                'text-decoration':'none',
+                'color':'white'
+            },
+        }
+
         return (
-            <div class="tab">
-                <Link to="home">主页</Link>
-                <Link to="book">预约</Link>
-                <Link to="mine">我的</Link>
+            <div style={footerStyle.tab}>
+                <Link to="home" style={footerStyle.item}>主页</Link>
+                <Link to="book" style={footerStyle.item}>预约</Link>
+                <Link to="mine" style={footerStyle.item}>我的</Link>
             </div>
         );
     }
--- src/app/views/home/index.js
+++ src/app/views/home/index.js
@@ -4,10 +4,30 @@ import BottomTab from '../../common/bottom-tab.js';
 
 export default class Home extends Component {
     render() {
+        var headerStyle={
+            'position':'fixed',
+            'width':'100%',
+            'height':'32px',
+            'background':'mediumaquamarine',
+            'top':0,
+            'left':0,
+            'text-align':'center',
+            'color':'white',
+            'padding-top':'8px'
+        };
+        var contentStyle={
+            'position':'absolute',
+            'top':'40px',
+            'width':'100%',
+            'bottom':'40px',
+            'left':0,
+            'margin':0,
+            'padding':0
+        }
         return (
             <div>
-                <h3>Home</h3>
-                <ul>
+                <div style={headerStyle}>Home</div>
+                <ul style={contentStyle}>
                     <img src={require("../../../images/logo.png")}/>
                 </ul>
                 <BottomTab/>
